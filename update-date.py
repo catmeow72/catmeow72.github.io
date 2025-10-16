d2e225c21294f8b709dbb147f5330322fc4a0d50
Katie Hall <catmeow@complecwaft.com>
2025-10-16 08:54:03 -0700

    Improve update-date.py and allow it to update _config.yml

--- update-date.py
+++ update-date.py
@@ -5,6 +5,16 @@ from os import path
 import tempfile
 from datetime import datetime, timezone, timedelta
 from subprocess import run
+import shutil
+
+if len(sys.argv) <= 1:
+    print("Error: Need path to file being updated.")
+    sys.exit(1)
+stem, ext = path.splitext(path.basename(sys.argv[1]))
+ext = ext.removeprefix(".")
+is_yaml = ext == "yaml" or ext == "yml"
+is_markdown = ext == "md"
+is_config = stem == "_config" and is_yaml
 scriptdir = path.realpath(path.dirname(__file__))
 curtime = datetime.now(timezone.utc).astimezone()
 fmt = curtime.strftime("%Y-%m-%d %H:%M:%S ")
@@ -24,14 +34,15 @@ fmt += "%02d%02d" % (offs_abs // 60, offs_abs % 60)
 pre = ""
 post = ""
 notes_path = path.join(scriptdir, sys.argv[1])
-found_frontmatter = False
+found_frontmatter = is_config
 found_date = False
+date_key = "last_updated" if is_config else "data"
 with open(notes_path, "rt") as f:
     f.seek(0, os.SEEK_END)
     len = f.tell()
     f.seek(0, os.SEEK_SET)
-    is_frontmatter = False
-    before_frontmatter = True
+    is_frontmatter = is_config
+    before_frontmatter = not is_config
     while f.tell() < len:
         line = f.readline()
         if before_frontmatter:
@@ -41,8 +52,8 @@ with open(notes_path, "rt") as f:
                 is_frontmatter = True
                 found_frontmatter = True
         elif is_frontmatter:
-            is_date = line.strip().startswith("date:")
-            if line.strip() == "---":
+            is_date = line.strip().startswith("%s:" % date_key)
+            if not is_config and line.strip() == "---":
                 is_frontmatter = False
                 if not found_date:
                     break
@@ -54,13 +65,14 @@ with open(notes_path, "rt") as f:
             else:
                 break
     post = f.read()
-if not found_frontmatter:
+if not found_frontmatter and not is_config:
     post = "---\n" + pre
     pre = "---\n"
-with tempfile.NamedTemporaryFile(suffix=".md", mode="wt", delete_on_close=False, delete=False) as f:
+with tempfile.NamedTemporaryFile(suffix=".%s" % ext, mode="wt", delete_on_close=False, delete=False) as f:
     fpath = f.name
     f.write(pre)
-    f.write("date: %s\n" % fmt)
+    f.write("%s: %s\n" % (date_key, fmt))
     f.write(post)
 
-os.rename(fpath, notes_path)
+shutil.copyfile(fpath, notes_path)
+os.remove(fpath)
